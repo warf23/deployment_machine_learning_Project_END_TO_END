05b0d7cfefffdc4ed3c3355fad1fcf09432c1f58
warf23 <simo.agrat1@gmail.com>
2023-07-16 19:41:37 +0100

    Update Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,6 +21,8 @@ from src.logger import logging
 
 from src.utils import save_object
 
+from src.utils import evaluate_models
+
 @dataclass
 class ModelTrainerConfig : 
     trained_model_file_path = os.path.join("artifacts", "trained_model.pkl")
@@ -52,6 +54,31 @@ class ModelTrainer :
                 
             }
             
+            model_report : dict = evaluate_models(X_train = X_train , y_train = y_train ,X_test = X_test,y_test = y_test
+                                                  models = models )
             
-        except:
-            pass
+            # get best model name
+            best_model_score = max(sorted(model_report.values()))
+            
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name] 
+            
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+            
+        except Exception as e :
+            raise CustomException(e, sys)
